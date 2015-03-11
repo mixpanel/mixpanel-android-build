ab73bf92e7bdcfa154276c6c7b0c34aa254415b4
Joe <joe@mixpanel.com>
2015-03-10 17:25:19 -0700

    TEST RUNS AND BUILD WORKS

--- tweaks-test/src/androidTest/java/com/mixpanel/android/tweaksbuildtest/TweaksFunctionalTest.java
+++ tweaks-test/src/androidTest/java/com/mixpanel/android/tweaksbuildtest/TweaksFunctionalTest.java
@@ -2,24 +2,21 @@ package com.mixpanel.android.tweaksbuildtest;
 
 import android.test.AndroidTestCase;
 
-import com.mixpanel.android.mpmetrics.MixpanelAPI;
 import com.mixpanel.android.mpmetrics.Tweaks;
 
 public class TweaksFunctionalTest extends AndroidTestCase {
     public void testThisRuns() {
-        MixpanelAPI mixpanel = MixpanelAPI.getInstance(getContext(), "TEST TOKEN0");
         assertTrue(true);
     }
 
     public void testTweakResults() {
         final TweakedObject subject = new TweakedObject();
-        MixpanelAPI mixpanel = MixpanelAPI.getInstance(getContext(), "TEST TOKEN1");
-        mixpanel.registerForTweaks(subject);
+        final Tweaks tweaks = new Tweaks();
+        tweaks.registerForTweaks(subject);
 
         assertEquals(subject.stringBanana, "Default Value");
         assertEquals(subject.doubleBanana, 0.0);
 
-        Tweaks tweaks = mixpanel.getTweaks();
         tweaks.set("bananas", 2.3);
         tweaks.set("bananas", "A B C");
 
